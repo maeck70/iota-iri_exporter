bcbf14295698714cc3e5bca3569abd971de64be5
Marcel van Eck <vaneck.marcel@gmail.com>
2018-02-21 01:05:08 -0800

    Change to Neighbors Test

--- active_neighbors_test.go
+++ active_neighbors_test.go
@@ -1,18 +1,16 @@
 package main
 
-import "fmt"
 import "testing"
 import "github.com/iotaledger/giota"
 
-
 type neighborTest struct {
 	tx_count int64
-	result float64
+	result   float64
 }
 
 func TestActiveNeighbor(t *testing.T) {
 
-	tx := []neighborTest {
+	tx := []neighborTest{
 		{tx_count: 100, result: 1},
 		{tx_count: 105, result: 1},
 		{tx_count: 110, result: 1},
@@ -30,24 +28,23 @@ func TestActiveNeighbor(t *testing.T) {
 
 	for i := range tx {
 		n := giota.Neighbor{
-				Address: giota.Address(addr), 
-				NumberOfNewTransactions: tx[i].tx_count,
+			Address:                 giota.Address(addr),
+			NumberOfNewTransactions: tx[i].tx_count,
 		}
 		neighbormatrix.historyInc()
 		neighbormatrix.register(n)
 
 		a = GetActiveNeighbor(addr)
 		if GetActiveNeighbor(addr) != tx[i].result {
-			t.Errorf("Expected Neighbor Active (1), got %v", a)	
+			t.Errorf("Expected Neighbor Active (1), got %v", a)
 		}
 	}
 
 }
 
-
 func TestActiveNeighbors(t *testing.T) {
 
-	tx := []neighborTest {
+	tx := []neighborTest{
 		{tx_count: 100, result: 1},
 		{tx_count: 105, result: 1},
 		{tx_count: 110, result: 1},
@@ -61,32 +58,31 @@ func TestActiveNeighbors(t *testing.T) {
 
 	addr := "foo.com"
 
-	nl := []giota.Neighbor {
+	nl := []giota.Neighbor{
 		{Address: giota.Address(addr)},
 	}
-
 
 	a := float64(0)
 
 	for i := range tx {
 		n := giota.Neighbor{
-				Address: giota.Address(addr), 
-				NumberOfNewTransactions: tx[i].tx_count,
+			Address:                 giota.Address(addr),
+			NumberOfNewTransactions: tx[i].tx_count,
 		}
 
 		if i == 4 {
 			a = GetActiveNeighbors(nl)
 			if a != tx[i].result {
-				t.Errorf("Expected One Active Neighbor, got %v", a)	
+				t.Errorf("Expected One Active Neighbor, got %v", a)
 			}
 		} else if i == 8 {
 			a = GetActiveNeighbors(nl)
 			if a != tx[i].result {
-				t.Errorf("Expected No Active Neighbors, got %v", a)	
+				t.Errorf("Expected No Active Neighbors, got %v", a)
 			}
 		} else {
 			neighbormatrix.historyInc()
-			neighbormatrix.register(n)			
+			neighbormatrix.register(n)
 		}
 	}
 }
