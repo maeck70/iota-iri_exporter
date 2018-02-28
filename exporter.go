b50bc8bc5d47f3f3ca35de6a8df4c556851915b0
Marcel van Eck <vaneck.marcel@gmail.com>
2018-02-27 21:57:53 -0800

    Confirmation Time Tracking

--- exporter.go
+++ exporter.go
@@ -37,7 +37,7 @@ import (
 
 // Version is set during build to the git Describe version
 // (semantic version)-(commitish) form.
-var Version = "0.4.1"
+var Version = "0.4.2"
 
 var (
 	listenAddress    = kingpin.Flag("web.listen-address", "Address to listen on for web interface and telemetry.").Default(":9311").String()
